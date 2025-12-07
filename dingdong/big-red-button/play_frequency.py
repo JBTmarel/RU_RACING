8b975981e7f61a62b567b9ffaa96985f84277a60
JBTmarel <a.orribjarnason@gmail.com>
2025-12-07 17:21:44 +0000

    lakjalskjdalskjd

---
+++ dingdong/big-red-button/play_frequency.py
@@ -0,0 +1,39 @@
+# Source - https://stackoverflow.com/a
+# Posted by jfs, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-06, License - CC BY-SA 3.0
+
+#!/usr/bin/env python
+"""Play a fixed frequency sound."""
+from __future__ import division
+import math
+
+from pyaudio import PyAudio # sudo apt-get install python{,3}-pyaudio
+
+try:
+    from itertools import izip
+except ImportError: # Python 3
+    izip = zip
+    xrange = range
+
+def sine_tone(frequency, duration, volume=1, sample_rate=22050):
+    n_samples = int(sample_rate * duration)
+    restframes = n_samples % sample_rate
+
+    p = PyAudio()
+    stream = p.open(format=p.get_format_from_width(1), # 8bit
+                    channels=1, # mono
+                    rate=sample_rate,
+                    output=True)
+    s = lambda t: volume * math.sin(2 * math.pi * frequency * t / sample_rate)
+    samples = (int(s(t) * 0x7f + 0x80) for t in xrange(n_samples))
+    for buf in izip(*[samples]*sample_rate): # write several samples at a time
+        stream.write(bytes(bytearray(buf)))
+
+    # fill remainder of frameset with silence
+    stream.write(b'\x80' * restframes)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+sine_tone(24000, 2, 0, 96000)
